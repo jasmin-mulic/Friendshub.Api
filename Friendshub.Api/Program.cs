5411a3a59c49e2c1928e2d191200fd66bbd948fb
jasmin-mulic <jasmin.mulic2025@gmail.com>
2025-10-07 13:57:58 +0200

    Removing wwwroot from repo

--- Friendshub.Api/Program.cs
+++ Friendshub.Api/Program.cs
@@ -7,10 +7,14 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+}); ;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
